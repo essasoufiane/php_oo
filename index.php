bfd0162587075183d155d053f2b44bb15a4ac51b
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-10 15:18:51 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// $tab['couleur'] = "bleu";
+// $tab['type'] = "plume";
+// $tab['prix'] = 5;
+
+// $stylo = (object) $tab;
+
+//  print_r($stylo);
+
+//  $ballonFoot = new stdClass;
+//  $ballonFoot->type='Football';
+//  $ballonFoot->marque='nike';
+//  $ballonFoot->taille=5;
+//  $ballonFoot->status='crevé';
+
+//  var_dump($ballonFoot);
+
+//  echo "hey le ballon de football, " . $ballonFoot->marque . " est " .$ballonFoot->status . ',Ismael !' . "c'etait un taille " .  $ballonFoot->taille . "c'est ca Issac ?"; 
+
+ 
+//  ---------syntaxe object operator----------
+ $ballonFoot = new stdClass;
+ $ballonFoot->type='Football';
+ $ballonFoot->marque='nike';
+ $ballonFoot->taille=5;
+ $ballonFoot->status='crevé';
