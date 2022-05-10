9464ad42217fca742997e34118f4a7fee729dbaf
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-10 18:00:21 +0200

    fin_de_journée

--- index.php
+++ index.php
@@ -19,8 +19,51 @@
 
  
 //  ---------syntaxe object operator----------
- $ballonFoot = new stdClass;
- $ballonFoot->type='Football';
- $ballonFoot->marque='nike';
- $ballonFoot->taille=5;
- $ballonFoot->status='crevé';
+//  $ballonFoot = new stdClass;
+//  $ballonFoot->type='Football';
+//  $ballonFoot->marque='nike';
+//  $ballonFoot->taille=5;
+//  $ballonFoot->status='crevé';
+
+//  var_dump(property_exists($ballonFoot, 'status'));
+//  if (!property_exists($ballonFoot, 'prix')) {
+//     $ballonFoot->prix=5;
+//     echo ($ballonFoot->prix);
+//  }
+
+//  var_dump(get_declared_classes());
+
+class Ballon {
+    public $marque;
+    public $sport;
+
+    function lancer() {
+        echo "vous avez lancé le ballon ! <br>";
+    }
+}
+
+$ballonFoot = new Ballon();
+$ballonFoot->marque ='Nike';
+$ballonFoot->sport ='Rugby';
+
+$ballonRugby = new Ballon();
+$ballonRugby->marque ="Gilbert TM";
+$ballonRugby->sport ="Rugby";
+
+
+// echo "sport $ballonFoot->sport <br> marque: $ballonFoot->marque";
+// echo "<br>";
+// echo "sport $ballonRugby->sport <br> marque: $ballonRugby->marque";
+
+$ballonFoot->lancer();
+
+class User {
+
+    public $nom;
+    public $age;
+
+    function tchater($message){
+        echo " - " . $message . "<br>";
+    }
+
+}
